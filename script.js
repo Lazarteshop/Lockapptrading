52084e3571f61e69a73766456e89e024bca9f25f
lazarte <75070893+Lazarteshop@users.noreply.github.com>
2025-12-23 09:30:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function onSubmit() {
 
     /* Option 1 */ 
 	/* Use the following line of code to go directly to another page when the correct code is entered and the "submit" button is clicked. */
-          /*if (document.getElementById('password').value == '1234') {window.location.href = 'http:google.com'; }else{ alert('Access Denied, Please try again');}*/
+          /*if (document.getElementById('password').value == 'xxxxa') {window.location.href = 'http:google.com'; }else{ alert('Access Denied, Please try again');}*/
     
     /* Option 2 */
     /* Use this line of code to show an "Access Granted" linkable button the user must click after entering the correct code and clicking the "submit" button. */
     /* (the new button that appears must also be clicked to go to the new page.) */ 
-            if (document.getElementById('password').value == '1234') {window.button2.style.display="block"; }
+            if (document.getElementById('password').value == 'xxxxa') {window.button2.style.display="block"; }
             else{ 
                 alert('Access Denied, Please try again');
             }
@@ -24,6 +24,6 @@ function onSubmit() {
 // the "maxlength" on the index.htm page to match. 
 
 // You can also change the web page in the "href" to another page. 
-// Simply replace "https://www.freecodecamp.org/" with your desired page. 
+// Simply replace "https://tradezonexxxxa.netlify.app//" with your desired page. 
 
 // You can also change the "alert" text above to what you want to say.
